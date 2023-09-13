fb91997df34a9fbf834988c014d0e8edb3fa157c
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2023-09-13 12:02:46 -0700

    Tipos de datos
    - Definir matrices asociativas
    - Definir e instanciar clases

--- tipos_de_datos.php
+++ tipos_de_datos.php
@@ -25,8 +25,10 @@ echo "b=".$b;
 // iniciar desde la posición hasta n 0..length-1
 // Ej: Matriz para registrar preguntas (solo un tipo de dato cadena)
 $pregunta1 = array("pregunta1?","resp1","resp2","resp3","resp2","Materia1");
-$pregunta2 = ["pregunta2?","resp1","resp2","resp3","resp2","Materia1"];
+$pregunta2 = ["pregunta2?","resp1","resp2","resp3","resp2","Materia2"];
 
+// para acceder a los datos de una array se hace por medio de su índice
+// $pregunta1[índice numérico] los índices va de 0 hasta length-1
 // imprimir el nombre de la materia de la pregunta1?
 echo "Materia de la pregutna1: ".$pregunta1[5];
 echo "\n"; // solo funciona en la terminal
@@ -39,5 +41,32 @@ var_dump($mixed);
 // internal arrays
 $internal = [$pregunta1, $pregunta2, $mixed];
 var_dump($internal);
+// imprimir el nombre de la materia de la pregunta2 desde el array $internal?
+echo "\n";
+echo $internal[1][5];
 
-// TODO: Associative Arrays
+// Associative Arrays
+// mysqli_query("sql"); fetch_assoc() fetch() fetch_object()
+$alumno = array("nombre"=>"Paulina", "apellidos"=>"Rodriguez Hurtado");
+// imprimir el nombre completo del alumno(a)
+echo $alumno["nombre"]." ".$alumno["apellidos"];
+
+// TODO: Definir una clase para tener un tipo de dato Alumno (StudlyCaps PascalCase)
+class Alumno
+{
+    public $Nombre;
+    public $Apellidos;
+
+    public function nombreCompleto()
+    {
+        return $this->Nombre." ".$this->Apellidos;
+    }
+}
+
+// Crear un objeto de tipo alumno (instanciar una clase)
+$alumno2 = new Alumno();
+$alumno2->Nombre = "Ashley";
+$alumno2->Apellidos = "Machado Velarde";
+// imprimir nombre completo de alumno2
+// echo $alumno2->Nombre." ".$alumno2->Apellidos;
+echo $alumno2->nombreCompleto();

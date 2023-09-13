04202940be1fb3a27ed435382ce5c6b9b3a5d640
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2023-09-13 12:28:15 -0700

    Configuración del proyecto
    - Archivo de configuración para cumplir con PSR-4

---
+++ config.inc.php
@@ -0,0 +1,9 @@
+<?php
+// Archivo utilizado para definir constantes y configuraciones
+// generales del proyecto, como el nombre de la aplicación
+
+// constante para definir el nombre de la aplicación
+define("APP_NAME", "Control Escolar - CETis108");
+
+// constante para definir la ruta (Path Ej: C:\users\miproyecto\)
+define("APP_DIR", __DIR__ . "/");
